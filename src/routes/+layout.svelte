7e7c2995558ce21facae761af37be6df4ff16c5f
ThorneWolf <thornecanyon@gmail.com>
2022-11-25 18:26:24 -0800

    touch ups

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
 	import '../app.css';
 </script>
 
-<slot />
+<div class="navbar bg-base-100 shadow-md">
+	<a href="/" class="btn btn-ghost normal-case text-xl">Page Title</a>
+</div>
+<div class="p-4">
+	<slot />
+</div>
